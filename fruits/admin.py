378455013c416006382253dfcb6abb7fb8444622
Bishal <bisalgt@gmail.com>
2019-10-03 11:06:08 +0545

    cleaning and restarting

--- fruits/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import Fruits
-
-admin.site.register(Fruits)
--- grocery_shop/settings.py
+++ grocery_shop/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'fruits.apps.FruitsConfig',
-    'seasons.apps.SeasonsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
