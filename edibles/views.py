6900c2b3415505a95904c178ba1b2ae1c27bf2dd
Bishal <bisalgt@gmail.com>
2019-10-04 23:42:59 +0545

    showing db contents in html page

--- edibles/views.py
+++ edibles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from edibles.models import Fruit, Season, Month
+
+
+def home(request):
+    fruit_objects = Fruit.objects.all()
+    season_objects = Season.objects.all()
+    month_objects = Month.objects.all()
+
+    context = {'fruit_objects': fruit_objects, 'season_objects':season_objects, 'month_objects':month_objects}
+
+    return render(request, 'home.html', context)
